4f585024263b8601b4d24f37ca3fe6bace67a3a0
Marko Punčec <mpuncec46@gmail.com>
2026-04-02 16:55:41 +0200

    Added logic
    I renamed and reordered some values. Added helper values to make logic cleaner and easier to follow. Logic is ready for v1.0

--- src/components/oneItem.jsx
+++ src/components/oneItem.jsx
@@ -18,48 +18,61 @@ const getTax = (havePremium) => {
 
 function OneItem({ itemType, isArtifact, tier, enchant }) {
   const [res1Amount, setRes1Amount] = useState(0);
-  const [res1Value, setRes1Value] = useState(0);
-
+  const [res1Price, setRes1Price] = useState(0);
   const [res2Amount, setRes2Amount] = useState(0);
-  const [res2Value, setRes2Value] = useState(0);
-
-  const [artifactValue, setArtifactValue] = useState(0);
-
-  const [gameItemValue, setGameItemValue] = useState(0);
-  const [returnRate, setReturnRate] = useState(0);
-
-  const [targetAmount, setTargetAmount] = useState(1);
-  const [itemPrice, setItemPrice] = useState(0);
+  const [res2Price, setRes2Price] = useState(0);
+  const [artifactPrice, setArtifactPrice] = useState(0);
+  const [itemValue, setItemValue] = useState(0); // in-game constant value
+  const [returnRate, setReturnRate] = useState(24.8);
+  const [craftedItemPrice, setCraftedItemPrice] = useState(0);
   const [stationTax, setStationTax] = useState(1000);
+  const [craftingAmount, setCraftingAmount] = useState(1);
   const [havePremium, setHavePremium] = useState(true);
+  const [journalEmptyPrice, setJournalEmptyPrice] = useState(0);
+  const [journalFullPrice, setJournalFullPrice] = useState(0);
 
-  const res1Total = res1Amount * res1Value;
-  const res2Total = res2Amount * res2Value;
-  const rawTotal = Number(res1Total) + Number(res2Total) + Number(artifactValue); // raw cost of resources, tells you basically nothing
+  // calculated helper values:
 
-  //
-
-  const tax = getTax(havePremium);
-  const orderTax = 0.025;
-  const fullTax = tax + orderTax; // full market tax for selling and sell order
-
-  const nutritionForOne = gameItemValue * 0.1125; // nutrition needed to craft one item
-  const craftCostForOne = (Number(res1Total) + Number(res2Total)) * (1 - (Number(returnRate)/100))
-    + Number(artifactValue)
-    + Number(nutritionForOne) * stationTax / 100; // resource value sum * (1 - RR) + artifact value + crafting station tax (double check this)
-
-  const profitForOne = itemPrice * (1 - (fullTax * 100)) - craftCostForOne; // selling price * (1 - tax) - craft cost
-  const profit = profitForOne * targetAmount; // total profit
-  const nutrition = nutritionForOne * targetAmount; // total nutrition needed for crafting station
-  const craftingFee = nutrition * stationTax / 100; // crafting fee sum, usage of station (double check this)
-
-  const fame = getFame(tier, enchant);
-  const journal = getJournal(tier);
+  const res1TotalPrice = res1Amount * res1Price;
+  const res2TotalPrice = res2Amount * res2Price;
+  const resPriceSum = res1TotalPrice + res2TotalPrice;
   
-  const fameTotal = fame * (Number(res1Amount) + Number(res2Amount)) * targetAmount; // total fame gained without premium
-  const journalAmount = fameTotal / journal; // how many journals you need for this craft
+  const sellTax = getTax(havePremium); // gets 4% or 8% based on premium status
+  const orderTax = 2.5; // always 2.5%
+  const fullTax = sellTax + orderTax; // full tax for using market, sell order (6.5% or 10.5%)
+  
+  const fullTaxDecimal = fullTax / 100; // use for cleaner expressions
+  const returnRateDecimal = returnRate / 100; // use for cleaner expressions
+  
+  const nutritionForOne = itemValue * 0.1125; // nutrition needed for crafting one item
+  const craftingFeeForOne = nutritionForOne * stationTax / 100; // crafting fee for crafting one item
+  
+  const fameCoefficient = getFame(tier, enchant); // it works, trust me
+  const journal = getJournal(tier); // fame required to fill one journal
 
-  const requiredReturnRate = null; // my thing, math on paper, needs tax added for precision
+  const craftCostForOne = (Number(resPriceSum) * (1 - returnRateDecimal)) + Number(artifactPrice) + Number(craftingFeeForOne);
+  const craftedItemPriceWithTax = craftedItemPrice * (1 - fullTaxDecimal);
+  const journalFullPriceWithTax = journalFullPrice * (1 - fullTaxDecimal);
+  
+  // final values to display:
+  const nutritionTotal = nutritionForOne * craftingAmount; // total nutrition needed for crafting
+  const craftingFeeTotal = Math.round(nutritionTotal * stationTax / 100); // total crafting fee amount
+
+  const profitForOne = craftedItemPriceWithTax - craftCostForOne;
+  const profitFromCrafted = profitForOne * craftingAmount; // profit from selling crafted items
+
+  const fameTotal = fameCoefficient * (Number(res1Amount) + Number(res2Amount)) * craftingAmount; // total fame gained without premium
+  const journalAmount = fameTotal / journal; // how many journals you can fill with this craft
+  const profitFromJournal = (journalFullPriceWithTax - journalEmptyPrice) * journalAmount; // profit from filling and selling journals
+
+  const profitTotal = profitFromCrafted + profitFromJournal; // final profit, most important value, depends on everything
+
+  // extra displayed values:
+  
+  const rawTotal = Number(resPriceSum) + Number(artifactPrice); // raw cost of resources, tells you basically nothing
+
+  const profitFromJournalForOne = profitFromJournal / craftingAmount;
+  const requiredReturnRate = 1 - ((craftedItemPriceWithTax + profitFromJournalForOne - artifactPrice - craftingFeeForOne)/resPriceSum); // return rate required to break even, I cooked
 
   return (
     <div className="flex flex-col items-start border-r border-zinc-500 pr-1 mb-4">
@@ -81,8 +94,8 @@ function OneItem({ itemType, isArtifact, tier, enchant }) {
         <input
           type="number"
           placeholder="Value"
-          value={res1Value === 0 ? "" : res1Value}
-          onChange={(e) => setRes1Value(e.target.value)}
+          value={res1Price === 0 ? "" : res1Price}
+          onChange={(e) => setRes1Price(e.target.value)}
           className="bg-zinc-100 text-black w-24 px-2 py-1 rounded"
         />
       </div>
@@ -101,8 +114,8 @@ function OneItem({ itemType, isArtifact, tier, enchant }) {
         <input
           type="number"
           placeholder="Value"
-          value={res2Value === 0 ? "" : res2Value}
-          onChange={(e) => setRes2Value(e.target.value)}
+          value={res2Price === 0 ? "" : res2Price}
+          onChange={(e) => setRes2Price(e.target.value)}
           className="bg-zinc-100 text-black w-24 px-2 py-1 rounded"
         />
       </div>
@@ -112,8 +125,8 @@ function OneItem({ itemType, isArtifact, tier, enchant }) {
         <input
           type="number"
           placeholder="..."
-          value={artifactValue === 0 ? "" : artifactValue}
-          onChange={(e) => setArtifactValue(e.target.value)}
+          value={artifactPrice === 0 ? "" : artifactPrice}
+          onChange={(e) => setArtifactPrice(e.target.value)}
           className="bg-zinc-100 text-black w-24 px-2 py-1 rounded"
         />
       </div>
@@ -121,9 +134,9 @@ function OneItem({ itemType, isArtifact, tier, enchant }) {
       <hr className="border-t w-full border-zinc-500" />
 
       <div>
-        {res2Total === 0 && artifactValue === 0 ? "" : `${formatNumber(res1Total)} `} 
-        {res2Total === 0 ? "" : ` + ${formatNumber(res2Total)} `}
-        {artifactValue === 0 ? "" : ` + ${formatNumber(artifactValue)} `}
+        {res2TotalPrice === 0 && artifactPrice === 0 ? "" : `${formatNumber(res1TotalPrice)} `} 
+        {res2TotalPrice === 0 ? "" : ` + ${formatNumber(res2TotalPrice)} `}
+        {artifactPrice === 0 ? "" : ` + ${formatNumber(artifactPrice)} `}
         = <span className="text-amber-300">{formatNumber(rawTotal)}</span>
       </div>
 
@@ -134,8 +147,8 @@ function OneItem({ itemType, isArtifact, tier, enchant }) {
         <input
           type="number"
           placeholder="..."
-          value={gameItemValue === 0 ? "" : gameItemValue}
-          onChange={(e) => setGameItemValue(e.target.value)}
+          value={itemValue === 0 ? "" : itemValue}
+          onChange={(e) => setItemValue(e.target.value)}
           className="bg-zinc-100 text-black w-24 px-2 py-1 rounded"
         />
       </div>
@@ -159,8 +172,8 @@ function OneItem({ itemType, isArtifact, tier, enchant }) {
         <input
           type="number"
           placeholder="..."
-          value={targetAmount === 0 ? "" : targetAmount}
-          onChange={(e) => setTargetAmount(e.target.value)}
+          value={craftingAmount === 0 ? "" : craftingAmount}
+          onChange={(e) => setCraftingAmount(e.target.value)}
           className="bg-zinc-100 text-black w-24 px-2 py-1 rounded"
         />
       </div>
@@ -170,8 +183,8 @@ function OneItem({ itemType, isArtifact, tier, enchant }) {
         <input
           type="number"
           placeholder="..."
-          value={itemPrice === 0 ? "" : itemPrice}
-          onChange={(e) => setItemPrice(e.target.value)}
+          value={craftedItemPrice === 0 ? "" : craftedItemPrice}
+          onChange={(e) => setCraftedItemPrice(e.target.value)}
           className="bg-zinc-100 text-black w-24 px-2 py-1 rounded"
         />
       </div>
@@ -202,14 +215,21 @@ function OneItem({ itemType, isArtifact, tier, enchant }) {
       <div className="flex flex-row gap-2 justify-center items-center mb-1">
         <label>Profit for one:</label>
         <div className="">
-          {profitForOne}
+          {`${formatNumber(craftedItemPrice * (1 - fullTax))} (${fullTax * 100}% tax) - ${formatNumber(craftCostForOne)} = ${formatNumber(profitForOne)}`}
+        </div>
+      </div>
+
+      <div className="flex flex-row gap-2 justify-center items-center mb-1">
+        <label>Crafting fee:</label>
+        <div>
+          {`${formatNumber(craftingFeeTotal)} (${nutritionTotal})`}
         </div>
       </div>
 
       <div className="flex flex-row gap-2 justify-center items-center mb-1">
         <label>Profit (with tax):</label>
         <div>
-          {profit}
+          {`${formatNumber(profitFromCrafted)} (${fullTax * 100}% tax)`}
         </div>
       </div>
     </div>

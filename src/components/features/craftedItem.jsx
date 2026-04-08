e217ecbe4470bec3231dda7610898d4a5b3eb19f
Marko Punčec <mpuncec46@gmail.com>
2026-04-08 18:37:39 +0200

    Refactoring pt.4
    Moved useState values into one object for easier handling between components in CratedItem component.
    -MP

--- src/components/features/craftedItem.jsx
+++ src/components/features/craftedItem.jsx
@@ -7,73 +7,82 @@ import { craftingSimulationWorstCase } from "../../utils/craftingSimulation";
 import { formatNumber, getFameCoefficient, getJournal, getTax } from "../../utils/constantGetters";
 
 function CraftedItem({ itemType, isArtifact, tier, enchant, onDelete, onDuplicate }) {
-  // * group related state into objects
-  const [res1Amount, setRes1Amount] = useState(0);
-  const [res1Price, setRes1Price] = useState(0);
-  const [res2Amount, setRes2Amount] = useState(0);
-  const [res2Price, setRes2Price] = useState(0);
-  const [artifactPrice, setArtifactPrice] = useState(0);
-  const [craftedItemPrice, setCraftedItemPrice] = useState(0);
-  const [returnRate, setReturnRate] = useState(24.8);
-  const [craftingAmount, setCraftingAmount] = useState(1);
-  const [itemValue, setItemValue] = useState(0); // in-game constant value
-  const [stationTax, setStationTax] = useState(1000);
-  const [havePremium, setHavePremium] = useState(true);
-  const [journalEmptyPrice, setJournalEmptyPrice] = useState(0);
-  const [journalFullPrice, setJournalFullPrice] = useState(0);
+  const [values, setValues] = useState({
+    tier: tier,
+    enchant: enchant,
+    res1Amount: 0,
+    res1Price: 0,
+    res2Amount: 0,
+    res2Price: 0,
+    artifactPrice: 0,
+    craftedItemPrice: 0,
+    returnRate: 24.8,
+    craftingAmount: 1,
+    itemValue: 0,
+    stationTax: 1000,
+    havePremium: true,
+    journalEmptyPrice: 0,
+    journalFullPrice: 0
+  });
 
-  // * move all calculations into a helper
+  const handleSetValues = (field, val) => {
+    setValues((prev) => ({
+      ...prev,
+      [field]: val
+    }))
+  }
+
   // calculated helper values:
-  const res1TotalPrice = res1Amount * res1Price;
-  const res2TotalPrice = res2Amount * res2Price;
+  const res1TotalPrice = values.res1Amount * values.res1Price;
+  const res2TotalPrice = values.res2Amount * values.res2Price;
   const resPriceSum = res1TotalPrice + res2TotalPrice;
   
-  const sellTax = getTax(havePremium); // gets 4% or 8% based on premium status
+  const sellTax = getTax(values.havePremium); // gets 4% or 8% based on premium status
   const orderTax = 2.5; // always 2.5%
   const fullTax = sellTax + orderTax; // full tax for using market, sell order (6.5% or 10.5%)
   
   const fullTaxDecimal = fullTax / 100; // use for cleaner expressions
-  const returnRateDecimal = returnRate / 100; // use for cleaner expressions
+  const returnRateDecimal = values.returnRate / 100; // use for cleaner expressions
   
-  const nutritionForOne = itemValue * 0.1125; // nutrition needed for crafting one item
-  const craftingFeeForOne = nutritionForOne * stationTax / 100; // crafting fee for crafting one item
+  const nutritionForOne = values.itemValue * 0.1125; // nutrition needed for crafting one item
+  const craftingFeeForOne = nutritionForOne * values.stationTax / 100; // crafting fee for crafting one item
   
-  const fameCoefficient = getFameCoefficient(tier, enchant); // it works, trust me
-  const journal = getJournal(tier); // fame required to fill one journal
+  const fameCoefficient = getFameCoefficient(values.tier, values.enchant); // it works, trust me
+  const journal = getJournal(values.tier); // fame required to fill one journal
 
-  const craftCostForOne = (resPriceSum * (1 - returnRateDecimal)) + artifactPrice + craftingFeeForOne;
-  const craftedItemPriceWithTax = craftedItemPrice * (1 - fullTaxDecimal);
-  const journalFullPriceWithTax = journalFullPrice * (1 - fullTaxDecimal);
+  const craftCostForOne = (resPriceSum * (1 - returnRateDecimal)) + values.artifactPrice + craftingFeeForOne;
+  const craftedItemPriceWithTax = values.craftedItemPrice * (1 - fullTaxDecimal);
+  const journalFullPriceWithTax = values.journalFullPrice * (1 - fullTaxDecimal);
 
   // final values to display:
-  const nutritionTotal = nutritionForOne * craftingAmount; // total nutrition needed for crafting
-  const craftingFeeTotal = Math.round(nutritionTotal * stationTax / 100); // total crafting fee amount
+  const nutritionTotal = nutritionForOne * values.craftingAmount; // total nutrition needed for crafting
+  const craftingFeeTotal = Math.round(nutritionTotal * values.stationTax / 100); // total crafting fee amount
   
   const profitForOne = craftedItemPriceWithTax - craftCostForOne;
-  const profitFromCrafted = profitForOne * craftingAmount; // profit from selling crafted items
+  const profitFromCrafted = profitForOne * values.craftingAmount; // profit from selling crafted items
   
-  const fameTotal = fameCoefficient * (res1Amount + res2Amount) * craftingAmount; // total fame gained without premium
+  const fameTotal = fameCoefficient * (values.res1Amount + values.res2Amount) * values.craftingAmount; // total fame gained without premium
   const journalAmount = fameTotal / journal; // how many journals you can fill with this craft
-  const profitFromJournal = (journalFullPriceWithTax - journalEmptyPrice) * journalAmount; // profit from filling and selling journals
+  const profitFromJournal = (journalFullPriceWithTax - values.journalEmptyPrice) * journalAmount; // profit from filling and selling journals
   
   const profitTotal = profitFromCrafted + profitFromJournal; // final profit, most important value, depends on everything
   
-  const res1AmountTotal = res1Amount * craftingAmount;
-  const res2AmountTotal = res2Amount * craftingAmount;
+  const res1AmountTotal = values.res1Amount * values.craftingAmount;
+  const res2AmountTotal = values.res2Amount * values.craftingAmount;
  
   const [res1AmountTotalWithReturnRate, res2AmountTotalWithReturnRate, buttonPressAmount] = 
-    craftingAmount <= 1 ? [res1Amount, res2Amount, 1] : craftingSimulationWorstCase(craftingAmount, returnRateDecimal, res1AmountTotal, res1Amount, res2AmountTotal, res2Amount);
+    values.craftingAmount <= 1 ? [values.res1Amount, values.res2Amount, 1] : craftingSimulationWorstCase(values.craftingAmount, returnRateDecimal, res1AmountTotal, values.res1Amount, res2AmountTotal, values.res2Amount);
 
   // extra displayed values:
-  const rawTotal = resPriceSum + artifactPrice; // raw cost of resources, tells you basically nothing
+  const rawTotal = resPriceSum + values.artifactPrice; // raw cost of resources, tells you basically nothing
 
-  const profitFromJournalForOne = profitFromJournal / craftingAmount;
-  const requiredReturnRate = 1 - ((craftedItemPriceWithTax + profitFromJournalForOne - artifactPrice - craftingFeeForOne)/resPriceSum); // return rate required to break even, I cooked
+  const profitFromJournalForOne = profitFromJournal / values.craftingAmount;
+  const requiredReturnRate = 1 - ((craftedItemPriceWithTax + profitFromJournalForOne - values.artifactPrice - craftingFeeForOne)/resPriceSum); // return rate required to break even, I cooked
 
   return (
     <div className="flex flex-row flex-wrap">
       <div className="flex flex-col items-start border-r border-zinc-500 pr-1 mr-2 mb-4">
-        <div className="text-lg text-amber-300 underline italic">{itemType.charAt(0).toUpperCase() + itemType.slice(1)} {tier}.{enchant} {isArtifact ? "(artifact)" : ""}</div>
+        <div className="text-lg text-amber-300 underline italic">{itemType.charAt(0).toUpperCase() + itemType.slice(1)} {values.tier}.{values.enchant} {isArtifact ? "(artifact)" : ""}</div>
         
         <button className="text-black bg-amber-300 rounded px-1 py-1" onClick={onDelete}>
           Delete
@@ -86,16 +95,16 @@ function CraftedItem({ itemType, isArtifact, tier, enchant, onDelete, onDuplicat
         <div className="flex flex-row gap-2 justify-center items-center mb-1" >
           <InputWithLabel
             labelText="Resource 1:"
-            value={res1Amount}
+            value={values.res1Amount}
             placeholder="Num"
-            setterFunction={setRes1Amount}
+            setterFunction={(val) => handleSetValues("res1Amount", val)}
             extra="X"
             tooltip={<Tooltip text="e.g. 16 x 4,400"/>}
           />
           <InputWithLabel
-            value={res1Price}
+            value={values.res1Price}
             placeholder="Price"
-            setterFunction={setRes1Price}
+            setterFunction={(val) => handleSetValues("res1Price", val)}
           />
         </div>
 
@@ -103,16 +112,16 @@ function CraftedItem({ itemType, isArtifact, tier, enchant, onDelete, onDuplicat
         <div className="flex flex-row gap-2 justify-center items-center mb-1" >
           <InputWithLabel
             labelText="Resource 2:"
-            value={res2Amount}
+            value={values.res2Amount}
             placeholder="Num"
-            setterFunction={setRes2Amount}
+            setterFunction={(val) => handleSetValues("res2Amount", val)}
             extra="X"
             tooltip={<Tooltip text="e.g. 8 x 5,100"/>}
           />
           <InputWithLabel
-            value={res2Price}
+            value={values.res2Price}
             placeholder="Price"
-            setterFunction={setRes2Price}
+            setterFunction={(val) => handleSetValues("res2Price", val)}
           />
         </div>
         }
@@ -120,8 +129,8 @@ function CraftedItem({ itemType, isArtifact, tier, enchant, onDelete, onDuplicat
         { isArtifact &&
         <InputWithLabel
           labelText="Artifact price:"
-          value={artifactPrice}
-          setterFunction={setArtifactPrice}
+          value={values.artifactPrice}
+          setterFunction={(val) => handleSetValues("artifactPrice", val)}
         />
         }
 
@@ -129,22 +138,22 @@ function CraftedItem({ itemType, isArtifact, tier, enchant, onDelete, onDuplicat
 
         <InputWithLabel
           labelText="Market price:"
-          value={craftedItemPrice}
-          setterFunction={setCraftedItemPrice}
+          value={values.craftedItemPrice}
+          setterFunction={(val) => handleSetValues("craftedItemPrice", val)}
           tooltip={<Tooltip text="e.g. sell order price (silver)"/>}
         />
 
         <InputWithLabel
           labelText="Return rate:"
-          value={returnRate}
-          setterFunction={setReturnRate}
+          value={values.returnRate}
+          setterFunction={(val) => handleSetValues("returnRate", val)}
           extra="%"
         />
 
         <InputWithLabel
           labelText="Crafting amount:"
-          value={craftingAmount}
-          setterFunction={setCraftingAmount}
+          value={values.craftingAmount}
+          setterFunction={(val) => handleSetValues("craftingAmount", val)}
           tooltip={<Tooltip text="how many of this specific item"/>}
         />
 
@@ -152,23 +161,23 @@ function CraftedItem({ itemType, isArtifact, tier, enchant, onDelete, onDuplicat
 
         <InputWithLabel
           labelText="Item value:"
-          value={itemValue}
-          setterFunction={setItemValue}
+          value={values.itemValue}
+          setterFunction={(val) => handleSetValues("itemValue", val)}
           tooltip={<Tooltip text="in-game value in description of items"/>}
         />
 
         <InputWithLabel
           labelText="Crafting station tax:"
-          value={stationTax}
-          setterFunction={setStationTax}
+          value={values.stationTax}
+          setterFunction={(val) => handleSetValues("stationTax", val)}
           tooltip={<Tooltip text="max 1000"/>}
         />
 
         <InputWithLabel
           labelText="Premium:"
           type="checkbox"
-          value={havePremium}
-          setterFunction={setHavePremium}
+          value={values.havePremium}
+          setterFunction={(val) => handleSetValues("havePremium", val)}
           tooltip={<Tooltip text="Do you have premium?"/>}
         />
 
@@ -176,14 +185,14 @@ function CraftedItem({ itemType, isArtifact, tier, enchant, onDelete, onDuplicat
 
         <InputWithLabel
           labelText="Empty journal price:"
-          value={journalEmptyPrice}
-          setterFunction={setJournalEmptyPrice}
+          value={values.journalEmptyPrice}
+          setterFunction={(val) => handleSetValues("journalEmptyPrice", val)}
         />
 
         <InputWithLabel
           labelText="Full journal price:"
-          value={journalFullPrice}
-          setterFunction={setJournalFullPrice}
+          value={values.journalFullPrice}
+          setterFunction={(val) => handleSetValues("journalFullPrice", val)}
         />
 
       </div>
@@ -192,9 +201,9 @@ function CraftedItem({ itemType, isArtifact, tier, enchant, onDelete, onDuplicat
         <ValueWithLabel
           value={
             <>
-              {res2TotalPrice === 0 && artifactPrice === 0 ? undefined : formatNumber(res1TotalPrice)}
+              {res2TotalPrice === 0 && values.artifactPrice === 0 ? undefined : formatNumber(res1TotalPrice)}
               {res2TotalPrice === 0 ? undefined : " + " + formatNumber(res2TotalPrice)}
-              {artifactPrice === 0 ? undefined : " + " + (formatNumber(artifactPrice))} = {""}
+              {values.artifactPrice === 0 ? undefined : " + " + (formatNumber(values.artifactPrice))} = {""}
               <span className="text-amber-300">{formatNumber(rawTotal)} (raw cost)</span>
             </>
           }
